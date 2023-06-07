9cb3bb3be181151de0ff2251393e52fba2e3ea05
신용화 <yong@sin-yonghwaui-MacBookAir.local>
2023-06-07 18:52:26 +0900

    add necessary files

---
+++ Deepface/deepface/basemodels/Resnet34_normhead.py
@@ -0,0 +1,54 @@
+import os
+import gdown
+import tensorflow as tf
+from deepface.commons import functions
+
+# pylint: disable=unsubscriptable-object
+
+# --------------------------------
+# dependency configuration
+
+tf_version = int(tf.__version__.split(".", maxsplit=1)[0])
+
+if tf_version == 1:
+    from keras.engine import training
+    import keras
+    from keras.layers import (
+        ZeroPadding2D,
+        Input,
+        Conv2D,
+        BatchNormalization,
+        PReLU,
+        Add,
+        Dropout,
+        Flatten,
+        Dense,
+    )
+else:
+    from tensorflow.python.keras.engine import training
+    from tensorflow import keras
+    from tensorflow.keras.layers import (
+        ZeroPadding2D,
+        Input,
+        Conv2D,
+        BatchNormalization,
+        PReLU,
+        Add,
+        Dropout,
+        Flatten,
+        Dense,
+    )
+# --------------------------------
+
+
+# url = "https://drive.google.com/uc?id=1LVB3CdVejpmGHM28BpqqkbZP5hDEcdZY"
+
+
+def loadModel(
+    url="https://github.com/serengil/deepface_models/releases/download/v1.0/arcface_weights.h5",
+):
+    model = keras.models.load_model("/content/drive/MyDrive/chang/weights/normhead/temp.h5")
+
+    return model
+
+
